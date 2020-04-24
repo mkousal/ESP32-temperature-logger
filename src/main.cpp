38fc4585ddc2812417cefdc2793c33ac8d8009ed
mkousal <mkousal1@gmail.com>
2020-04-24 14:53:22 +0200

    Remove unused code

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,9 @@
 #include <WiFi.h>
 #include "InfluxArduino.hpp"
 #include "LM75A.h"
-#include "credentials.h"
+#include "credentials.h"  // Visit README.md for more info about this file
 
-LM75A lm(true, true, true);
+LM75A lm(true, true, true); // address pins in brackets (0x4F)
 InfluxArduino influx;
 
 //for settings look into credentials.h file
@@ -14,33 +14,30 @@ InfluxArduino influx;
 RTC_DATA_ATTR unsigned long count = 0;  //store count variable into RTC memory for use after waking up from deep sleep
 #define TEMP_MEAS 50 //number of temperature measurement to calculate average temp --> better accuracy
 
-
-#define LM_ADDR 0x4F
-
 void setup() {
-  Serial.begin(115200);
-  WiFi.begin(WIFI_NAME, WIFI_PASS);
-  int wifiAttempts = 0;
-  pinMode(19, OUTPUT);  // WiFi status LED
-  digitalWrite(19, 1);
-  while(WiFi.status() != WL_CONNECTED){
+  Serial.begin(115200);                       // Initialize serial line with 115200 baudraute
+  WiFi.begin(WIFI_NAME, WIFI_PASS);           // Begin WiFi connection with SSID and password typed in 'credentials.h' file
+  int wifiAttempts = 0;                       // Prompt to store WiFi connection attempts
+  pinMode(19, OUTPUT);                        // WiFi status LED, when ON -> WiFi is connecting
+  digitalWrite(19, 1);                        // Turn WiFi status LED on
+  while(WiFi.status() != WL_CONNECTED){       // Wait for connecting ESP32 to WiFi
     delay(500);
     Serial.print(".");
     wifiAttempts++;
-    if(wifiAttempts == 60){
+    if(wifiAttempts == 60){                   // If WiFi connection fails after 60 attempts -> restart ESP
       esp_restart();
     }
   }
-  digitalWrite(19, 0);
+  digitalWrite(19, 0);                        // Turn off WiFi status LED
   Serial.println("WiFi connected!");
-  influx.configure(INFLUX_DATABASE, INFLUX_IP);
+  influx.configure(INFLUX_DATABASE, INFLUX_IP);  
   influx.authorize(INFLUX_USER, INFLUX_PASS);
   Serial.print("Using HTTPS: ");
   Serial.println(influx.isSecure());
 
   float temp = 0;
   for (int i = 0; i != TEMP_MEAS; i++){
-    temp += lm.getTemperatureInDegrees();
+    temp += lm.getTemperatureInDegrees();   // Get temperature from connected LM75 sensor
     delay(100);
   }
   temp = temp/TEMP_MEAS;
@@ -63,19 +60,5 @@ void setup() {
 }
 
 void loop() {
-  // float temp = lm.getTemperatureInDegrees();
-  // unsigned long startTime = micros();
-  // count++;
-  // char tags[32];
-  // char fields[32];
-  // sprintf(tags, "new_tag=Yes");
-  // sprintf(fields, "count=%d,temp=%0.1f",count,temp);
-  // bool writeSuccessful = influx.write(INFLUX_MEASUREMENT,tags,fields);
-  // if(!writeSuccessful){
-  //   Serial.print("error: ");
-  //   Serial.println(influx.getResponse());
-  // }
 
-  // while ((micros() - startTime) < DELAY_TIME_US)
-  // {}
 }
