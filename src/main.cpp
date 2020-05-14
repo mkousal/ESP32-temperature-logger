fda00499621f7b84bde2d6c11055fef5c9f3d18a
Martin Kousal <mkousal1@gmail.com>
2020-05-14 12:11:13 +0200

    Add turning on/off OLED display using #define

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,40 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <WiFi.h>
+#include "settings.h"
 #include "InfluxArduino.hpp"
 #include "LM75A.h"
 #include "credentials.h"  // Visit README.md for more info about this file
 #include "U8g2lib.h"
 
-U8X8_SSD1306_128X32_UNIVISION_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE, /* clock=*/ SCL, /* data=*/ SDA);   // pin remapping with ESP8266 HW I2C
-
+U8X8_SSD1306_128X32_UNIVISION_HW_I2C u8x8(U8X8_PIN_NONE, SCL, SDA);
 
 LM75A lm(true, true, true); // address pins in brackets (0x4F)
 InfluxArduino influx;
 
-//for settings look into credentials.h file
+// for WiFi and database settings look into 'credentials.h' file
+// other settings can be found in 'settings.h' file
 
-#define DELAY_TIME_US 300 * 1000 * 1000 //5 minutes
-// RTC_DATA_ATTR unsigned long count = 0;  //store count variable into RTC memory for use after waking up from deep sleep
 RTC_DATA_ATTR bool oledInit = false;
-#define TEMP_MEAS 50 //number of temperature measurement to calculate average temp --> better accuracy
-
-// #define DEBUG                                // Enable Serial line output
 
 void initMCU() {
   #ifdef DEBUG
-  Serial.begin(115200);                       // Initialize serial line with 115200 baudraute
+    Serial.begin(115200);                     // Initialize serial line with 115200 baudraute
   #endif
   WiFi.begin(WIFI_NAME, WIFI_PASS);           // Begin WiFi connection with SSID and password typed in 'credentials.h' file
   pinMode(19, OUTPUT);                        // WiFi status LED, when ON -> WiFi is connecting
   pinMode(23, INPUT_PULLUP);                  // Pin for disabling measurement
-  if (!oledInit) {
-    u8x8.begin();
-    oledInit = true;
-  }
-  else {
-    u8x8_gpio_Init(u8x8.getU8x8());
-    u8x8_cad_Init(u8x8.getU8x8());
-    u8x8_gpio_SetReset(u8x8.getU8x8(), 1);
-  }
+  #ifdef OLED
+    if (!oledInit) {
+      u8x8.begin();
+      oledInit = true;
+    }
+    else {
+      u8x8_gpio_Init(u8x8.getU8x8());
+      u8x8_cad_Init(u8x8.getU8x8());
+      u8x8_gpio_SetReset(u8x8.getU8x8(), 1);
+    }
+    #endif
 }
 
 void goSleep(long us) {
@@ -53,16 +51,26 @@ float getTemperature() {
     temp += lm.getTemperatureInDegrees();   // Get temperature from connected LM75 sensor
     delay(100);
   }
-  temp = temp/TEMP_MEAS;
+  temp = temp / TEMP_MEAS;
+  temp = ((round(temp*2))/2);
   return temp;
+}
+
+void displayTemperature(float temp) {
+  char oledMSG[8];
+  sprintf(oledMSG, "%0.1f°C", temp);
+  u8x8.setFont(u8x8_font_inr21_2x4_f);
+  uint8_t x = (u8x8.getCols() - (u8x8.getUTF8Len(oledMSG))*2) / 2;
+  u8x8.drawUTF8(x, 0, oledMSG);
 }
 
 void setup() {
   initMCU();
   float temp = getTemperature();
+  #ifdef OLED
+    displayTemperature(temp);
+  #endif
   if (digitalRead(23) == 0){
-    u8x8.setFont(u8x8_font_inr21_2x4_f);
-    u8x8.printf("%.1f °C", ((round(temp*2))/2));
     goSleep(1000000);
   }
 
@@ -84,9 +92,6 @@ void setup() {
   Serial.print("Using HTTPS: ");
   Serial.println(influx.isSecure());
 
-  u8x8.setFont(u8x8_font_inr21_2x4_f);
-  u8x8.print(lm.getTemperatureInDegrees());
-  u8x8.print("°C");
   char tags[32];
   char fields[32];
   sprintf(tags, "new_tag=Yes");
---
+++ src/settings.h
@@ -0,0 +1,4 @@
+// #define DEBUG                               // Enable serial line
+#define OLED                                // Enable OLED display
+#define TEMP_MEAS 50                        // number of temperature measurement to calculate average temp --> better accuracy
+#define DELAY_TIME_US 300 * 1000 * 1000     // Deep sleep time -> 5 minutes
